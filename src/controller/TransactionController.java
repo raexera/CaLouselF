e353a646bb17b661c97ce1cce38b06a41b3aada2
raexera <raexera@gmail.com>
2024-12-15 00:30:42 +0700

    feat: Transaction and Wishlist Controller

--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -1,5 +1,58 @@
 package controller;
 
-public class TransactionController {
+import database.DatabaseConnector;
+import model.Transaction;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionController {
+	private static TransactionController instance;
+	private final DatabaseConnector db;
+
+	private TransactionController() {
+		db = DatabaseConnector.getInstance();
+	}
+
+	public static TransactionController getInstance() {
+		if (instance == null) {
+			instance = new TransactionController();
+		}
+		return instance;
+	}
+
+	public void createTransaction(Transaction transaction) {
+		String query = String.format("INSERT INTO Transactions (BuyerID, ItemID, TotalPrice) VALUES (%d, %d, %.2f)",
+				transaction.getBuyerID(), transaction.getItemID(), transaction.getTotalPrice());
+		db.execute(query);
+
+		String wishlistQuery = String.format("DELETE FROM Wishlist WHERE BuyerID = %d AND ItemID = %d",
+				transaction.getBuyerID(), transaction.getItemID());
+		db.execute(wishlistQuery);
+
+		System.out.println("Transaction created successfully. Item removed from wishlist if it existed.");
+	}
+
+	public List<Transaction> viewTransactionHistory(int buyerID) {
+		String query = String.format("SELECT t.TransactionID, t.BuyerID, t.ItemID, t.TotalPrice, t.CreatedAt, "
+				+ "i.ItemName, i.Category, i.Size " + "FROM Transactions t " + "JOIN Items i ON t.ItemID = i.ItemID "
+				+ "WHERE t.BuyerID = %d", buyerID);
+		ResultSet rs = db.execQuery(query);
+		List<Transaction> transactions = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				Transaction transaction = new Transaction(rs.getInt("TransactionID"), rs.getInt("BuyerID"),
+						rs.getInt("ItemID"), rs.getDouble("TotalPrice"));
+				System.out.printf("Transaction ID: %d, Item Name: %s, Category: %s, Size: %s, Price: %.2f, Date: %s%n",
+						transaction.getTransactionID(), rs.getString("ItemName"), rs.getString("Category"),
+						rs.getString("Size"), rs.getDouble("TotalPrice"), rs.getTimestamp("CreatedAt"));
+				transactions.add(transaction);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return transactions;
+	}
 }
--- src/controller/WishlistController.java
+++ src/controller/WishlistController.java
@@ -1,5 +1,57 @@
 package controller;
 
-public class WishlistController {
+import database.DatabaseConnector;
+import model.Wishlist;
+import model.Item;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WishlistController {
+	private static WishlistController instance;
+	private final DatabaseConnector db;
+
+	private WishlistController() {
+		db = DatabaseConnector.getInstance();
+	}
+
+	public static WishlistController getInstance() {
+		if (instance == null) {
+			instance = new WishlistController();
+		}
+		return instance;
+	}
+
+	public void addItemToWishlist(Wishlist wishlist) {
+		String query = String.format("INSERT INTO Wishlist (BuyerID, ItemID) VALUES (%d, %d)", wishlist.getBuyerID(),
+				wishlist.getItemID());
+		db.execute(query);
+		System.out.println("Item added to wishlist successfully.");
+	}
+
+	public List<Item> viewWishlist(int buyerID) {
+		String query = String
+				.format("SELECT i.* FROM Wishlist w JOIN Items i ON w.ItemID = i.ItemID WHERE w.BuyerID = %d", buyerID);
+		ResultSet rs = db.execQuery(query);
+		List<Item> wishlistItems = new ArrayList<>();
+		try {
+			while (rs.next()) {
+				wishlistItems.add(new Item(rs.getInt("ItemID"), 0, rs.getString("ItemName"), rs.getString("Category"),
+						rs.getString("Size"), rs.getDouble("Price"), rs.getString("Status"),
+						rs.getString("DeclineReason")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return wishlistItems;
+	}
+
+	public void removeItemFromWishlist(Wishlist wishlist) {
+		String query = String.format("DELETE FROM Wishlist WHERE WishlistID = %d AND BuyerID = %d",
+				wishlist.getWishlistID(), wishlist.getBuyerID());
+		db.execute(query);
+		System.out.println("Item removed from wishlist successfully.");
+	}
 }
